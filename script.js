8a8f2cd81d6303cf952dac96c5e84ee93a410d79
nibezo <nibezo.cs@gmail.com>
2023-03-22 14:35:52 +0300

    fix: add answer on /start command

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const { Configuration, OpenAIApi } = require('openai');
 
 // For safety API keys stores in api.js file
-const apiOpenAi = require("./api.js")
-const apiTelegram = require("./api.js")
+const apiOpenAi = require('./api.js');
+const apiTelegram = require('./api.js');
 
-apiKeyOpenAi = apiOpenAi["apiOpenAi"]["api"]
-apiKeyTelegram = apiTelegram["apiTelegram"]["api"]
-
+apiKeyOpenAi = apiOpenAi['apiOpenAi']['api'];
+apiKeyTelegram = apiTelegram['apiTelegram']['api'];
 
 const Telegram = require('@yuva1422/telegram.js');
 const client = new Telegram.Client();
@@ -17,24 +16,30 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
 });
 
-client.on('message', msg => {
-  prompt = msg.content;
-  const configuration = new Configuration({
-    apiKey: apiKeyOpenAi,
-  });
-  const openai = new OpenAIApi(configuration);
-  
-  (async() => {
-    const gptResponse = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: prompt,
-    temperature: 1,
-    max_tokens: 350,
-    })
-    console.log(prompt)
-    answer = gptResponse.data.choices[0].text;
-    msg.reply(answer);
-  })()
+client.on('message', (msg) => {
+  if (msg.content === '/start') {
+    msg.reply(
+      'Это бот @nibezo для внутренних тестов. Напиши что-то, бот ответит с небольшой задержкой (лучше писать на английском, чтобы ответы были длинее). '
+    );
+  } else {
+    prompt = msg.content;
+    const configuration = new Configuration({
+      apiKey: apiKeyOpenAi,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    (async () => {
+      const gptResponse = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: prompt,
+        temperature: 1,
+        max_tokens: 350,
+      });
+      console.log(prompt);
+      answer = gptResponse.data.choices[0].text;
+      msg.reply(answer);
+    })();
+  }
 });
 
 client.login(apiKeyTelegram);
